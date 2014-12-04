38ae5d9fcb8f361f5e6111225022636807fcb4b8
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2014-12-04 20:40:33 +0100

    Configure devise routes. Add an admin controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {registrations: "registrations"}
+  get 'admin/dashboard'
+
+  devise_for :users, controllers: { registrations: "registrations" },
+                     path: 'admin/users',
+                     path_names: { sign_in: 'login', sign_out: 'logout' }
   root to: 'visitors#index'
 end
