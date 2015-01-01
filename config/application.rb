685a9695d93bcc52c2374076ecb0d2cbf67dab85
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2015-01-02 00:01:15 +0100

    Configure RSpec.

--- config/application.rb
+++ config/application.rb
@@ -14,8 +14,8 @@ module Napp
         fixtures: true,
         view_specs: false,
         helper_specs: false,
-        routing_specs: false,
-        controller_specs: false,
+        routing_specs: true,
+        controller_specs: true,
         request_specs: false
       g.fixture_replacement :factory_girl, dir: "spec/factories"
     }
