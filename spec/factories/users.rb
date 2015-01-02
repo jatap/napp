83a0f1ead11f9fd4ffc0d31432d9575e6d52cc09
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2015-01-02 02:18:40 +0100

    Fix current specs.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,4 +1,26 @@
 FactoryGirl.define do
   factory :user do
+    fullname { Faker::Name.name.downcase }
+    # email { Faker::Internet.email }
+    sequence(:email) { |n| "person#{n}@example.com" }
+    password 'password'
+    password_confirmation 'password'
+  end
+
+  factory :user_with_editor_role, parent: :user do
+    roles { [build(:role, name: 'editor')] }
+  end
+
+  factory :user_with_user_role, parent: :user do
+    roles { [build(:role, name: 'user')] }
+  end
+
+  factory :user_with_admin_role, parent: :user do
+    roles { [build(:role, name: 'admin')] }
+  end
+
+  factory :user_without_password, parent: :user_with_guest_role do
+    password nil
+    password_confirmation nil
   end
 end
