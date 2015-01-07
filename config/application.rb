d97539859920e0c510a7a4bc7711444abaa55047
Julio Antúnez Tarín <julio.antunez.tarin@gmail.com>
2015-01-07 20:34:27 +0100

    Add rails helpers generators.

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module Napp
       g.test_framework :rspec,
         fixtures: true,
         view_specs: false,
-        helper_specs: false,
+        helper_specs: true,
         routing_specs: true,
         controller_specs: true,
         request_specs: false
